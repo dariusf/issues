05cc3fc06432e33a4ba5260134a185b3fc2559fa
Darius Foo <darius.foo.tw@gmail.com>
2015-08-15 15:42:10 +0800

    Removed unnecessary stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,19 +15,8 @@ public class Main {
         }
     }
 
-    private static Optional<Expr> parse(String s) {
-        Expr result;
-        try {
-            parser p = new parser(new Lexer(new StringReader(s + "\n")));
-            result = (Expr) p.parse().value;
-        } catch (Throwable e) {
-            return Optional.empty();
-        }
-        return Optional.of(result);
-    }
-
     private static void test(String s) {
-        System.out.printf("%s => %s\n", s, parse(s));
+        System.out.println("hello");
     }
 
     public static void main(String[] args) {
